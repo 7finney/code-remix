706985d4306b31e2998ff83a365f67f186da3ffe
0mkar <0mkar@protonmail.com>
2021-03-03 02:02:13 +0530

    fetch & compile

--- src/plugins/fetch_compile.ts
+++ src/plugins/fetch_compile.ts
@@ -17,4 +17,18 @@ export default class FetchAndCompile extends CommandPlugin {
 		this.unresolvedAddresses = [];
     this.sourceVerifierNetWork = ['Main', 'Rinkeby', 'Ropsten', 'Goerli'];
   }
+  /**
+   * Fetch compiliation metadata from source-Verify from a given @arg contractAddress - https://github.com/ethereum/source-verify
+   * Put the artifacts in the file explorer
+   * Compile the code using Solidity compiler
+   * Returns compilation data
+   *
+   * @param {string} contractAddress - Address of the contrac to resolve
+   * @param {string} deployedBytecode - deployedBytecode of the contract
+   * @param {string} targetPath - Folder where to save the compilation arfefacts
+   * @return {CompilerAbstract} - compilation data targeting the given @arg contractAddress
+   */
+  async resolve (contractAddress, codeAtAddress, targetPath) {
+    console.log('should resolve');
+  }
 }
